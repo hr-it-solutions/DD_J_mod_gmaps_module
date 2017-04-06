e9ac1b77438f219e30b0243119f5ba98af069129
Florian Häusler <florian@didldu.com>
2017-04-06 19:03:17 +0200

    Added helper

---
+++ helper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @version    1-1-0-0 // Y-m-d 2017-04-06
+ * @author     HR IT-Solutions Florian Häusler https://www.hr-it-solutions.com
+ * @copyright  Copyright (C) 2011 - 2017 Didldu e.K. | HR IT-Solutions
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ **/
+
+defined('_JEXEC') or die;
+
+/**
+ * Helper for mod_dd_gmaps_module
+ *
+ * @since  Version 1.0.0.0
+ */
+class ModDD_GMaps_Module_Helper
+{
+
+}
