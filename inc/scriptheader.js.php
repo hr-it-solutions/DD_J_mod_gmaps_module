6c5c0449262ac2819f6b527930db532a2f434eb0
Florian Häusler <florian@didldu.com>
2017-10-21 18:45:28 +0200

    Code cleanup Info Windows functions added to display address or infowindows content Trigger Info Windows PupUp function improved

--- inc/scriptheader.js.php
+++ inc/scriptheader.js.php
@@ -50,14 +50,25 @@ function Sanitize_output($buffer)
 	return true;
 }
 
+/**
+ * @package    DD_GMaps_Module
+ * @author     HR IT-Solutions Florian Häusler <info@hr-it-solutions.com>
+ *
+ * Sanitize_output
+ * @sincer   Version 1.1.0.8
+ **/
 ob_start("Sanitize_output");
 ?>
+
 jQuery(document).ready(function () {
     init_default_itemsJS();
 });
+
 var home = new google.maps.LatLng(<?php echo $instance->paramLatLong($params); ?>),
     settingsClusterIcon = '<?php echo $instance->paramClusterMarkerImage($params); ?>',
     settingsZoomLevel = <?php  echo (int) $params->get('zoomlevel') ?>,
+
+    <?php // Build - Locations array ?>
     GMapsLocations = [
 		<?php
 		foreach ( $items as $i => $item ):
@@ -72,7 +83,8 @@ var home = new google.maps.LatLng(<?php echo $instance->paramLatLong($params); ?
 			}
 			else
 			{
-				$title_link = JRoute::_('index.php?option=com_dd_gmaps_locations&view=profile&id=' . (int) $item->id . ':' . htmlspecialchars($item->alias, ENT_QUOTES, 'UTF-8'));
+				$title_link = JRoute::_('index.php?option=com_dd_gmaps_locations&view=profile&id=' . (int) $item->id
+                    . ':' . htmlspecialchars($item->alias, ENT_QUOTES, 'UTF-8'));
 			}
 
 			$title      = '<a href="' . $title_link . '">' . $title . '</a>';
@@ -94,6 +106,28 @@ var home = new google.maps.LatLng(<?php echo $instance->paramLatLong($params); ?
 		}
 		?>
         {
+
+	        <?php
+            if ($params->get('only_extended_locations') != '1' && $item->id == 0)
+            {
+	            $infoContent = $title;
+                if(trim($params->get('infowindow_content')) != '')
+                {
+	                $infoContent = $params->get('infowindow_content') . $infoContent;
+                }
+                if ($params->get('infowindow_defaultaddress') == '1')
+                {
+                    $infoContent = $title . '<br>' . htmlspecialchars($item->street, ENT_QUOTES, 'UTF-8') .
+                        '<br>' . htmlspecialchars($item->location, ENT_QUOTES, 'UTF-8') . $infoContent;
+                }
+            }
+            else
+            {
+                $infoContent = $title . '<br>' . htmlspecialchars($item->street, ENT_QUOTES, 'UTF-8') .
+                    '<br>' . htmlspecialchars($item->location, ENT_QUOTES, 'UTF-8');
+            }
+            ?>
+
             id:<?php echo isset($item->id) ? $item->id : 0; ?>,
             key:<?php echo $i; ?>,
             lat:<?php echo $item->latitude; ?>,
@@ -104,14 +138,19 @@ var home = new google.maps.LatLng(<?php echo $instance->paramLatLong($params); ?
                 origin: new google.maps.Point(0, 0),
                 anchor: new google.maps.Point(0, 0)
             },
-            content: '<?php echo '<span class="info-content">' . $title . '<br>' . htmlspecialchars($item->street, ENT_QUOTES, 'UTF-8') . '<br>' . htmlspecialchars($item->location, ENT_QUOTES, 'UTF-8') . '</span>'; ?>'
+            content: '<?php echo '<span class="info-content">' . $infoContent . '</span>'; ?>'
         },<?php
 		endforeach; ?>
     ];
+    <?php // End - Loactions array ?>
+
 <?php // Initialize Map ?>
 var infowindow = new google.maps.InfoWindow();
 google.maps.event.addDomListener(window, 'load', initialize);
+
 <?php
+// Info windows
+
 // Geolocate info window launcher
 if ($input->get("geolocate", "STRING") == "locate")
 {
@@ -125,10 +164,20 @@ if ($input->get("geolocate", "STRING") == "locate")
 	echo "launchLocateInfoWindow($lat,$lng,'$content',$zoom,'$markertitle','$markericon');";
 }
 
+// Profile pages info window
 if ($input->get('profile_id') != 0)
 {
 	echo 'setTimeout(function(){
             var profileObj = jQuery.grep(GMapsLocations, function(e){ return e.id == ' . $input->get('profile_id', 0) . '; });
+            if(typeof profileObj[0] !== "undefined"){launchInfoWindow(profileObj[0].key)}
+          }, 800);';
+}
+
+// Default address info window
+elseif ($params->get('infowindow_opendefault') && $params->get('only_extended_locations') != '1')
+{
+	echo 'setTimeout(function(){
+            var profileObj = jQuery.grep(GMapsLocations, function(e){ return e.id == 0; });
             if(typeof profileObj[0] !== "undefined"){launchInfoWindow(profileObj[0].key)}
           }, 800);';
 }
