99cf477d8d0d7595e7a53c4fab2904534a2d5a15
Florian Häusler <florian@hr-it-solutions.com>
2020-08-28 10:08:10 +0200

    Joomla4 Migration

--- tmpl/default.php
+++ tmpl/default.php
@@ -9,14 +9,17 @@
 
 defined('_JEXEC') or die;
 
-JHtml::_('stylesheet', 'mod_dd_gmaps_module/dd_gmaps_module.css', array('version' => 'auto', 'relative' => true));
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Uri\Uri;
 
-JHtml::_('jQuery.Framework');
+HTMLHelper::_('stylesheet', 'mod_dd_gmaps_module/dd_gmaps_module.css', array('version' => 'auto', 'relative' => true));
+HTMLHelper::_('jquery.framework');
 
 ?>
 <div class="dd_gmaps_module">
 <?php if($params->get('eu_privay_mode') && $params->get('gdpr_cover')): ?>
-<style>#dd_gmaps {cursor: pointer; background: url("<?php echo JURI::base() . $params->get('gdpr_cover'); ?>"); background-size: cover;}</style>
+<style>#dd_gmaps {cursor: pointer; background: url("<?php echo Uri::base() . $params->get('gdpr_cover'); ?>"); background-size: cover;}</style>
 <?php endif; ?>
 	<?php
 	// If force_map_size is enabled
@@ -30,14 +33,14 @@ JHtml::_('jQuery.Framework');
 	if ($params->get('fullsize')): ?>
         <div id="dd_gmaps_fullsize" class="pull-left">
             <button id="toggleFullSize"
-                    class="btn fullsize-btn"><?php echo JText::_('MOD_DD_GMAPS_MODULE_FULLSIZE'); ?></button>
+                    class="btn fullsize-btn"><?php echo Text::_('MOD_DD_GMAPS_MODULE_FULLSIZE'); ?></button>
         </div>
 	<?php endif; ?>
     <div id="dd_gmaps">
         <?php if(!$params->get('eu_privay_mode')): ?>
-        <p class="dd_gmaps_loader"><?php echo JText::_('MOD_DD_GMAPS_MODULE_MAPS_PRELOADER'); ?></p>
+        <p class="dd_gmaps_loader"><?php echo Text::_('MOD_DD_GMAPS_MODULE_MAPS_PRELOADER'); ?></p>
         <?php else: ?>
-        <div id="dd_gmaps_gdpr_text"><?php echo JText::_('MOD_DD_GMAPS_MODULE_GDPR') . $params->get('gdpr_text'); ?></div>
+        <div id="dd_gmaps_gdpr_text"><?php echo Text::_('MOD_DD_GMAPS_MODULE_GDPR') . $params->get('gdpr_text'); ?></div>
         <?php endif; ?>
     </div>
     <div class="clear"></div>
